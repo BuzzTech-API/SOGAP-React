64f9b072d439159b4030e8d44a6062066c5bb229
Cauan Almeida <cauan.almeida@fatec.sp.gov.br>
2023-11-08 02:12:27 -0300

    Terminando o modal para filtrar os cards

--- src/components/Modal/ModalFilters.tsx
+++ src/components/Modal/ModalFilters.tsx
@@ -11,17 +11,23 @@ interface FilterSettings {
     setProcess?: React.Dispatch<React.SetStateAction<Process[]>>
     setSteps?: React.Dispatch<React.SetStateAction<Step[]>>
     setRequestForEvidence?: React.Dispatch<React.SetStateAction<RequestForEvidence[]>>
-    processes?: Array<Process>
+    setSelectedType?: React.Dispatch<React.SetStateAction<string[]>>
+    setFilteredSteps?: React.Dispatch<React.SetStateAction<Step[]>>
+    setFilteredRequestForEvidence?: React.Dispatch<React.SetStateAction<RequestForEvidence[]>>
+    selectedType?: string[]
     steps?: Array<Step>
     requestForEvidence?: Array<RequestForEvidence>
+    type: number
 }
 
-export const ModalFilter = ({ setProcess, processes, setSteps, steps, setRequestForEvidence, requestForEvidence }: FilterSettings) => {
+export const ModalFilter = ({ setProcess, steps, setFilteredSteps, setFilteredRequestForEvidence, requestForEvidence, setSelectedType, selectedType, type }: FilterSettings) => {
     const { isOpen, onOpen, onClose } = useDisclosure()
     const [selectedDates, setSelectedDates] = useState<string[]>([])
     const [selectedStatus, setSelectedStatus] = useState<string[]>([])
+    const [selectedPriority, setSelectedPriority] = useState<string[]>([])
     const [filterProcess, setFilterProcess] = useState(new Array<Process>())
-    const [selectedType, setSelectedType] = useState<string[]>([])
+    const [filterStep, setFilterStep] = useState(new Array<Step>())
+    const [filterRequestEvidence, setFilterRequestEvidence] = useState(new Array<RequestForEvidence>())
 
 
 
@@ -34,22 +40,22 @@ export const ModalFilter = ({ setProcess, processes, setSteps, steps, setRequest
             if (userContent && setProcess) {
                 setProcess(userContent.processes)
                 setFilterProcess(userContent.processes)
+                setFilterStep(userContent.steps)
+                setFilterRequestEvidence(userContent.requests)
 
-                if(setSteps && setRequestForEvidence)
-                    setSteps(userContent.steps)
-                if(setRequestForEvidence)
-                    setRequestForEvidence(userContent.requests)
+                if (setFilteredSteps && setFilteredRequestForEvidence)
+                    setFilteredSteps(userContent.steps)
+                if (setFilteredRequestForEvidence)
+                    setFilteredRequestForEvidence(userContent.requests)
             }
-        })();
-    }, [setProcess, setRequestForEvidence, setSteps])
+        })()
+    }, [setFilteredRequestForEvidence, setFilteredSteps, setProcess])
 
     const handleDateSelect = (date: string) => {
         if (selectedDates.includes(date)) {
             setSelectedDates(selectedDates.filter(x => x !== date))
-            console.log(selectedDates)
         } else {
             setSelectedDates([...selectedDates, date])
-            console.log(selectedDates)
         }
     }
 
@@ -61,20 +67,34 @@ export const ModalFilter = ({ setProcess, processes, setSteps, steps, setRequest
         }
     }
 
-    const handleTypeSelect = (type: string) => {
-        if (selectedType.includes(type)) {
-            setSelectedType(selectedType.filter(x => x !== type))
+    const handlePrioritySelect = (status: string) => {
+        if (selectedPriority.includes(status)) {
+            setSelectedPriority(selectedPriority.filter(x => x !== status))
         } else {
-            setSelectedType([...selectedType, type])
+            setSelectedPriority([...selectedPriority, status])
+        }
+    }
+
+    const handleTypeSelect = (type: string) => {
+        if (selectedType !== undefined && setSelectedType !== undefined) {
+            if (selectedType.includes(type)) {
+                setSelectedType(selectedType.filter(x => x !== type))
+            } else {
+                setSelectedType([...selectedType, type])
+            }
         }
     }
 
     const resetFilters = () => {
-        console.log(processes)
         if (setProcess)
             setProcess(filterProcess)
+        if (setFilteredSteps)
+            setFilteredSteps(filterStep)
+        if (setFilteredRequestForEvidence)
+            setFilteredRequestForEvidence(filterRequestEvidence)
         setSelectedDates([])
         setSelectedStatus([])
+        setSelectedPriority([])
         onClose()
     }
 
@@ -83,12 +103,39 @@ export const ModalFilter = ({ setProcess, processes, setSteps, steps, setRequest
         e.preventDefault()
         if (setProcess)
             setProcess(filterProcess)
+        if (setFilteredSteps)
+            setFilteredSteps(filterStep)
+        if (setFilteredRequestForEvidence)
+            setFilteredRequestForEvidence(filterRequestEvidence)
+
+
         const filteredProcesses = filterProcess.filter((process) => {
             return (
-                (selectedDates.length === 0 || selectedDates.includes(process.lastUpdate.toString())) &&
-                (selectedStatus.length === 0 || selectedStatus.includes(process.status))
-                )
+                (selectedDates.length === 0 || selectedDates.includes(process.endingDate.toString())) &&
+                (selectedStatus.length === 0 || selectedStatus.includes(process.status)) &&
+                (selectedPriority.length === 0 || selectedPriority.includes(process.priority))
+            )
         })
+        const filteredSteps = filterStep.filter((step) => {
+            return (
+                (selectedDates.length === 0 || selectedDates.includes(step.endDate.toString())) &&
+                (selectedStatus.length === 0 || selectedStatus.includes(step.status)) &&
+                (selectedPriority.length === 0 || selectedPriority.includes(step.priority))
+            )
+        })
+        const filteredRequests = selectedPriority.length === 0 ? filterRequestEvidence.filter((request) => {
+            return (
+                (selectedDates.length === 0 || selectedDates.includes(request.evidenceValidationDate.toString()))
+            )
+        }) : []
+        
+        if (setFilteredRequestForEvidence)
+            setFilteredRequestForEvidence(filteredRequests)
+
+
+        if (setFilteredSteps)
+            setFilteredSteps(filteredSteps)
+
         if (setProcess)
             setProcess(filteredProcesses)
         onClose()
@@ -105,7 +152,7 @@ export const ModalFilter = ({ setProcess, processes, setSteps, steps, setRequest
                 _hover={{ color: "#29784E", bg: "white" }}
                 onClick={onOpen}
             >Filtros</Button>
-            <ModalGeneric isOpen={isOpen} onClose={onClose} widthModal="50rem" heightModal="lg">
+            <ModalGeneric isOpen={isOpen} onClose={onClose} widthModal="auto" heightModal="auto">
                 <form onSubmit={filterProcesses}>
                     <Text
                         fontFamily={'Poppins'}
@@ -118,12 +165,15 @@ export const ModalFilter = ({ setProcess, processes, setSteps, steps, setRequest
                         mb={3}
                     >Filtros</Text>
                     <Flex
+                        direction={"column"}
+                        alignSelf={'center'}
                         justify={['column', 'row']}
+                        justifyContent={'center'}
                         align={'top'}>
                         <Box
                             width={'40%'}
                             key={1}
-                            >
+                        >
                             <Text
                                 alignSelf={'center'}
                                 fontFamily={'Poppins'}
@@ -136,51 +186,90 @@ export const ModalFilter = ({ setProcess, processes, setSteps, steps, setRequest
                                 maxHeight='10rem'
                                 minHeight='10rem'
                                 maxWidth={'15rem'}
-                                overflowY={'auto'}>
-                                {[...(filterProcess.map(process => process.lastUpdate))].filter((value,
+                                overflowY={'auto'}
+                                mb={1}>
+                                {[...(filterProcess.map(process => process.endingDate)),
+                                ...(type === 5001 ? filterStep.map(step => step.endDate) : []),
+                                ...(type === 5001 ? filterRequestEvidence.map(request => request.evidenceValidationDate) : [])].filter((value,
                                     index, self) => self.indexOf(value) === index).sort((a, b) =>
                                         new Date(a).getTime() - new Date(b).getTime()).map((date, key) => (
-                                            <Table key={key}>
+                                            <div key={key}>
                                                 <Checkbox size={"lg"}
                                                     isChecked={selectedDates.includes(date.toString())}
                                                     onChange={() => handleDateSelect(date.toString())}>
                                                     {date.toString()}
                                                 </Checkbox>
-                                            </Table>
+                                            </div>
                                         ))}
                             </Flex>
                         </Box>
 
-                        <Box key={2}>
-                            <Text
-                                alignSelf={'center'}
-                                fontFamily={'Poppins'}
-                                fontSize='1.3rem'
-                                fontStyle='normal'
-                                mb={1}
-                            >Status</Text>
-                            <Flex
-                                direction={"column"}
-                                alignSelf={'left'}
-                                justifyContent={'center'}
-                                mr={2}>
-                                <Checkbox size={"lg"}
-                                    isChecked={selectedStatus.includes("Não iniciado")}
-                                    onChange={() => handleStatusSelect("Não iniciado")}>
-                                    Não iniciado</Checkbox>
+                        {type === 5000 && (
+                            <Box key={2}>
+                                <Text
+                                    alignSelf={'center'}
+                                    fontFamily={'Poppins'}
+                                    fontSize='1.3rem'
+                                    fontStyle='normal'
+                                    mb={1}
+                                >Status</Text>
+                                <Flex
+                                    direction={"column"}
+                                    alignSelf={'left'}
+                                    justifyContent={'center'}
+                                    mr={2}
+                                    mb={1}>
+                                    <Checkbox size={"lg"}
+                                        isChecked={selectedStatus.includes("Não iniciado")}
+                                        onChange={() => handleStatusSelect("Não iniciado")}>
+                                        Não iniciado</Checkbox>
 
-                                <Checkbox size={"lg"}
-                                    isChecked={selectedStatus.includes("Em andamento")}
-                                    onChange={() => handleStatusSelect("Em andamento")}>
-                                    Em andamento</Checkbox>
+                                    <Checkbox size={"lg"}
+                                        isChecked={selectedStatus.includes("Iniciado")}
+                                        onChange={() => handleStatusSelect("Iniciado")}>
+                                        Iniciado</Checkbox>
 
 
-                                <Checkbox size={"lg"}
-                                    isChecked={selectedStatus.includes("Concluído")}
-                                    onChange={() => handleStatusSelect("Concluído")}>
-                                    Concluído</Checkbox>
-                            </Flex>
-                        </Box>
+                                    <Checkbox size={"lg"}
+                                        isChecked={selectedStatus.includes("Concluído")}
+                                        onChange={() => handleStatusSelect("Concluído")}>
+                                        Concluído</Checkbox>
+                                </Flex>
+                            </Box>
+                        )}
+                        {type === 5001 && (
+                            <Box key={4}>
+                                <Text
+                                    alignSelf={'center'}
+                                    fontFamily={'Poppins'}
+                                    fontSize='1.3rem'
+                                    fontStyle='normal'
+                                    mb={1}
+                                >Prioridade</Text>
+                                <Flex
+                                    direction={"column"}
+                                    alignSelf={'left'}
+                                    justifyContent={'center'}
+                                    mr={2}
+                                    mb={1}>
+                                    <Checkbox size={"lg"}
+                                        isChecked={selectedPriority.includes("Baixa")}
+                                        onChange={() => handlePrioritySelect("Baixa")}>
+                                        Baixa</Checkbox>
+
+                                    <Checkbox size={"lg"}
+                                        isChecked={selectedPriority.includes("Média")}
+                                        onChange={() => handlePrioritySelect("Média")}>
+                                        Média</Checkbox>
+
+
+                                    <Checkbox size={"lg"}
+                                        isChecked={selectedPriority.includes("Alta")}
+                                        onChange={() => handlePrioritySelect("Alta")}>
+                                        Alta</Checkbox>
+                                </Flex>
+                            </Box>
+                        )}
                         {(steps !== undefined || requestForEvidence !== undefined) && (
                             <Box key={3}>
                                 <Text
@@ -195,20 +284,20 @@ export const ModalFilter = ({ setProcess, processes, setSteps, steps, setRequest
                                     alignSelf={'left'}
                                     justifyContent={'center'}>
                                     <Checkbox size={"lg"}
-                                        isChecked={selectedType.includes("Processos")}
+                                        isChecked={selectedType?.includes("Processos")}
                                         onChange={() => handleTypeSelect("Processos")}>
                                         Processos</Checkbox>
 
                                     {steps !== undefined && (
                                         <Checkbox size={"lg"}
-                                            isChecked={selectedType.includes("Etapas")}
+                                            isChecked={selectedType?.includes("Etapas")}
                                             onChange={() => handleTypeSelect("Etapas")}>
                                             Etapas</Checkbox>
                                     )}
 
                                     {requestForEvidence !== undefined && (
                                         <Checkbox size={"lg"}
-                                            isChecked={selectedType.includes("Requisições de Evidência")}
+                                            isChecked={selectedType?.includes("Requisições de Evidência")}
                                             onChange={() => handleTypeSelect("Requisições de Evidência")}>
                                             Requisições de Evidência</Checkbox>
                                     )}
